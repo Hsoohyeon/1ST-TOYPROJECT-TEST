a193806a36eb25f3006792229098e595a1a60992
Hsoohyeon <soohyeon062800@gmail.com>
2023-12-29 22:54:20 +0900

    style: 1차 완료

--- src/components/pages/Main/main.jsx
+++ src/components/pages/Main/main.jsx
@@ -3,9 +3,12 @@ import "./main.scss";
 
 const Main = () => {
   return (
-    <div>
-      <div>hello! main</div>
-      <h1>hi </h1>
+    <div className="Container">
+      <div className="SubContainer">
+        <img className="Logo1" src="/images/Logo1.png" alt="Logo1" />
+        <h4 className="SubDes">당신의 취향에 맞는 전통주를 찾아드립니다!</h4>
+        <button className="StartBtn">시작하기</button>
+      </div>
     </div>
   );
 };
