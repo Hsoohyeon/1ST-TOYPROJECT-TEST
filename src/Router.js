5fd11d93bb05e8297fbaf62ac766cbe8a3ff6cae
Hsoohyeon <soohyeon062800@gmail.com>
2023-12-29 20:53:16 +0900

    setting commit2

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
+import Main from "./components/pages/Main/main";
 
 const Router = () => {
   return (
